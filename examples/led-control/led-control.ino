611d8068b0f6ea1c3b054364f4bcdfcb57f8f422
HoangHoi <hoanghoi1310@gmail.com>
2017-07-15 00:21:48 +0700

    final

--- examples/led-control/led-control.ino
+++ examples/led-control/led-control.ino
@@ -12,10 +12,15 @@ const char HexLookup[17] = "0123456789ABCDEF";
 
 String host = "192.168.2.109";
 int port = 3000;
-bool ledState = false;
+uint8_t ledState = 0;
+uint8_t r = 0;
+uint8_t g = 0;
+uint8_t b = 0;
+uint8_t ra = 0;
+uint8_t ga = 0;
+uint8_t ba = 0;
 
 SocketIOClient socket;
-DynamicJsonBuffer jsonBuffer;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setupNetwork() {
@@ -27,14 +32,60 @@ void setupNetwork() {
     }
 }
 
-void led(String data) {
-    JsonObject& root = jsonBuffer.parseObject(data);
-    Serial.println("[light] " + data);
-    String color = root["1"]["color"];
-    String status = root["1"]["status"];
-    // Serial.println(color);
-    // Serial.println(status);
-    // String r = strtoul(data.substring());
+void setLed() {
+    if (ledState) {
+        ra = r;
+        ga = g;
+        ba = b;
+    } else {
+        ra = 0;
+        ga = 0;
+        ba = 0;
+    }
+
+    pixels.setPixelColor(0, pixels.Color(ra, ga, ba));
+    pixels.show();
+}
+
+void emitStatus() {
+    String emitString = "{\"status\":";
+    emitString += ledState;
+    emitString += ",\"color\":{\"r\":";
+    emitString += r;
+    emitString += ",\"g\":";
+    emitString += g;
+    emitString += ",\"b\":";
+    emitString += b;
+    emitString += ",\"a\":1}}";
+
+    Serial.println(emitString);
+    socket.emit("led-status", emitString);
+}
+
+void changeLedState(String data) {
+    StaticJsonBuffer<500> jsonBuffer;
+    JsonArray& root = jsonBuffer.parse(data);
+    if (!root.success()) {
+        Serial.println("Parse json failed");
+        return;
+    }
+
+    Serial.println("[changeLed] " + data);
+    r = root[1]["color"]["r"];
+    g = root[1]["color"]["g"];
+    b = root[1]["color"]["b"];
+    ledState = root[1]["status"];
+
+    Serial.print("[changeLed] status: ");
+    Serial.println(ledState);
+    Serial.print("[changeLed] r: ");
+    Serial.println(r);
+    Serial.print("[changeLed] g: ");
+    Serial.println(g);
+    Serial.print("[changeLed] b: ");
+    Serial.println(b);
+    setLed();
+    emitStatus();
 }
 
 void setup() {
@@ -49,14 +100,11 @@ void setup() {
     pixels.begin();
 
     // socket.setChannel("feed-the-fish");
-    socket.on("led-change", led);
-
+    socket.on("led-change", changeLedState);
     socket.connect(host, port);
-    // socket.emit("room", "led-room");
+    emitStatus();
 }
 
 void loop() {
     socket.monitor();
-    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-    pixels.show();
 }
